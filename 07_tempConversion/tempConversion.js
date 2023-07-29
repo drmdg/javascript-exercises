64f6fce51abf6f8a7aff4088c4e3ce35a592a432
vboxuser <vboxuser@ubuntu.myguest.virtualbox.org>
2023-07-29 10:07:48 -0300

    Primeira parte concluida

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+
+  temp = (temp - 32)*(5/9);
+  temp=temp.toFixed(1);
+  temp=Number(temp);
+  return temp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  temp = temp * (9/5) + 32;
+  temp=temp.toFixed(1);
+  temp=Number(temp);
+  return temp;
+
 };
 
 // Do not edit below this line
