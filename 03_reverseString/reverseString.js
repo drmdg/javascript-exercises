b0188c9baf7388e036688f965e91414df996982b
vboxuser <vboxuser@ubuntu.myguest.virtualbox.org>
2023-07-29 09:46:47 -0300

    quase lá

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,14 @@
-const reverseString = function() {
+const reverseString = function(name) {
+    let splittedname="";
+
+    splittedname=name.split("");
+    name="";
+
+    for(let i= splittedname.length;i>0;i--){
+        name=name+splittedname[i-1];
+    }
+    return name;
+
 
 };
 
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
 const removeFromArray = function() {
 
+    let args= Array.from(arguments);
+
+    for(let index=1;index<args.length;index++){
+        for (let i=0; i< args[0].length;i++){
+            if(args[0][i]===args[index]){
+                args[0].splice(i,1);
+
+            }
+        }
+    }
+    return args[0];
 };
 
 // Do not edit below this line
