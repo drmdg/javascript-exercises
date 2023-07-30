8a26d2fd6f8bcf4dba30b4853a80d6289bd17220
vboxuser <vboxuser@ubuntu.myguest.virtualbox.org>
2023-07-30 18:45:18 -0300

    quase lá

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,50 @@
-const add = function() {
+const add = function(...args) {
+
+  let soma=0;
+  for(let i of args){
+    soma=soma+i;
+  }
+  return soma;
 	
 };
 
-const subtract = function() {
-	
+const subtract = function(...args) {
+  let sub=2*args[0];
+  for(let i of args){
+    sub=sub-i;
+  }
+  return sub;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let soma=0;
+  for(let i of arr){
+    soma= soma + i;
+  }
+  return soma;
+
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  let mult=1;
+  for(let i of arr){
+    mult= mult * i;
+  }
+  return mult;
 
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+    return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(fat) {
+	let result=1;
+  for(let i=1;i<=fat;i++){
+    result=i*result;
+  }
+  return result;
+
 };
 
 // Do not edit below this line
